1766f6ac0eab9a6d2a91296dde4349f24966c213
Dominik Winter <info@edge-project.org>
2016-04-11 23:15:47 +0200

    complete rewrite of web container

--- web/web-init.sh
+++ web/web-init.sh
@@ -10,7 +10,4 @@ HOST_GID=$(stat -c "%g" $WWW_DIR)
 usermod  -o -u $HOST_UID $WWW_USER
 groupmod -o -g $HOST_GID $WWW_GROUP
 
-sed -i "/Options/c\  Options Indexes FollowSymLinks" /etc/apache2/apache2.conf
-echo "date.timezone = UTC" > /usr/local/etc/php/conf.d/date_timezone.ini
-
-apache2-foreground
+service apache2 start

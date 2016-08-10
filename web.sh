9966aea47bfdfaf8c5d908ace8579f0f23c3c54c
Dominik Winter <info@edge-project.org>
2016-08-11 01:23:04 +0200

    added xhprof and xhgui to php7 image

--- web.sh
+++ web.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-WWW_DIR=/var/www/html
-WWW_USER=www-data
-WWW_GROUP=www-data
+if ! grep -q "stretch" /etc/issue; then
+    WWW_DIR=/var/www/html
+    WWW_USER=www-data
+    WWW_GROUP=www-data
 
-HOST_UID=$(stat -c "%u" $WWW_DIR)
-HOST_GID=$(stat -c "%g" $WWW_DIR)
+    sed -ie "
+        s/\${APACHE_RUN_USER}/$WWW_USER/g;
+        s/\${APACHE_RUN_GROUP}/$WWW_GROUP/g;
+    " /etc/apache2/apache2.conf
 
-usermod  -o -u $HOST_UID $WWW_USER
-groupmod -o -g $HOST_GID $WWW_GROUP
+    HOST_UID=$(stat -c "%u" $WWW_DIR)
+    HOST_GID=$(stat -c "%g" $WWW_DIR)
+
+    usermod  -o -u $HOST_UID $WWW_USER
+    groupmod -o -g $HOST_GID $WWW_GROUP
+else
+    service mongodb start
+fi
 
 service apache2 start
+
+# keep container running if apache fails
+tail -f /var/log/dmesg
